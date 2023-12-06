3d6085abb34190a44f1237be3ca2dfdbf0fca2f2
hyu-dev <hyu630115@gmail.com>
2023-12-06 17:57:54 +0900

    modify. 불필요한 기능 제거
    check permission

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,14 +1,10 @@
 import { LoadingTemplate } from "@components/templates/Loading";
 import { ToastListTemplate } from "@components/templates/ToastList";
-import { useCheckPermission } from "@hooks/useCheckPermission";
 import { router } from "@routers/router";
 import { memo } from "react";
 import { RouterProvider } from "react-router-dom";
 
 const App = memo(() => {
-  // 알림 권한 확인
-  useCheckPermission();
-
   return (
     <div className="relative mx-auto flex flex-col items-center w-[360px] min-h-[600px]">
       <LoadingTemplate />
