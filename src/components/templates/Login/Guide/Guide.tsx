068180c39155964232a146147999996544fd2ec9
hyu-dev <hyu630115@gmail.com>
2023-12-10 01:55:54 +0900

    modify. guide 수정

--- src/components/templates/Login/Guide/Guide.tsx
+++ src/components/templates/Login/Guide/Guide.tsx
@@ -5,14 +5,14 @@ import { useCallback } from "react";
 export const Guide = () => {
   const onClick = useCallback(() => {
     Alert.basic({
-      title: "Send notification once a day",
-      html: "It's simple to use.<br/>Administrators can create teams. Users can belong to teams and send notifications. That's it!<br/>If there is an error while using it, please report it to hyu630115@gmail.com.",
+      title: "Bug inquiry",
+      html: "Send it to hyu630115@gmail.com",
     });
   }, []);
 
   return (
     <Button type="button" className="text-guide-style text-sm" onClick={onClick}>
-      🍩 Do you want a guide?
+      🌀 bug report
     </Button>
   );
 };
